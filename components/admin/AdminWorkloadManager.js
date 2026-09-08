e79156f267d6058893aa2991d804f0052eb87d55
DIIC ZONE <diiczone@gmail.com>
2026-09-07 22:23:05 -0500

    ok

--- components/admin/AdminWorkloadManager.js
+++ components/admin/AdminWorkloadManager.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
     Clock, Users, Activity, AlertTriangle,
     BarChart3, Zap, Rocket, UserPlus,
