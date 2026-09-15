1ac2d91f9550ff6525b8f06f41f4ab75e519308d
DIIC ZONE <diiczone@gmail.com>
2026-09-15 00:27:33 -0500

    feat: exact match for verification

--- app/tiktokjo2GQaFAftpBLsMcjSMgfZX3YDEYGFIA.txt/route.js
+++ app/tiktokjo2GQaFAftpBLsMcjSMgfZX3YDEYGFIA.txt/route.js
@@ -1,1 +1,1 @@
-export async function GET() { return new Response('tiktok-developers-site-verification=jo2GQaFAftpBLsMcjSMgfZX3YDEYGFIA', { headers: { 'Content-Type': 'text/plain' } }); }
+export async function GET() { return new Response('tiktok-developers-site-verification=jo2GQaFAftpBLsMcjSMgfZX3YDEYGFlA', { headers: { 'Content-Type': 'text/plain' } }); }
